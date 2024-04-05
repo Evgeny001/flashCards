5c59db77e63f0cc4d1f50900b734a1e0a78cdaa6
leonraz58 <leonraz58@gmail.com>
2024-04-05 21:41:29 +0300

    feat: add tabs #16

---
+++ src/components/ui/tabs/tabs.tsx
@@ -0,0 +1,47 @@
+import { ComponentPropsWithoutRef } from 'react'
+
+import * as TabsRadix from '@radix-ui/react-tabs'
+
+import s from './tabs.module.scss'
+
+import { Typography } from '../typography'
+
+type Tab = {
+  disabled: boolean
+  value: string
+}
+
+export type Props = {
+  tabs: Tab[]
+  title?: string
+} & ComponentPropsWithoutRef<typeof TabsRadix.Root>
+
+export const Tabs = (props: Props) => {
+  const { tabs, title, ...rest } = props
+
+  return (
+    <div className={s.wrapperTabs}>
+      {title && (
+        <Typography as={'span'} className={s.title} variant={'body2'}>
+          {title}
+        </Typography>
+      )}
+      <TabsRadix.Root className={s.tabsRoot} {...rest}>
+        <TabsRadix.List className={s.tabsList}>
+          {tabs.map(tab => (
+            <>
+              <TabsRadix.Trigger
+                className={s.tabsTrigger}
+                disabled={tab.disabled}
+                key={tab.value}
+                value={tab.value}
+              >
+                {tab.value}
+              </TabsRadix.Trigger>
+            </>
+          ))}
+        </TabsRadix.List>
+      </TabsRadix.Root>
+    </div>
+  )
+}
