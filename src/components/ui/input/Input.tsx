a1d88657d001060fe52b4ec4d3be3922a544e0b4
Evgeny <info.elukas@gmail.com>
2024-04-14 20:57:15 +0300

    feat: add forwardRef, callback,icon display logic and icons Eye, EyeOff, Search, Close Refs: #12

--- src/components/ui/input/Input.tsx
+++ src/components/ui/input/Input.tsx
@@ -1,8 +1,17 @@
-import React, { ComponentPropsWithoutRef, KeyboardEvent, ReactNode } from 'react'
+import React, {
+  ChangeEvent,
+  ComponentPropsWithoutRef,
+  KeyboardEvent,
+  ReactNode,
+  forwardRef,
+} from 'react'
 
+import Eye from '@/assets/icons/Eye'
+import EyeOff from '@/assets/icons/EyeOff'
+import Search from '@/assets/icons/Search'
 import { Typography } from '@/components/ui/typography'
 
-export type PropsType = {
+export type InputProps = {
   errorMessage?: string
   iconLeft?: ReactNode
   iconRight?: ReactNode
@@ -14,45 +23,70 @@ export type PropsType = {
   type?: 'password' | 'search' | 'text'
 } & ComponentPropsWithoutRef<'input'>
 
-export const Input: React.FC<PropsType> = props => {
-  const {
-    className,
-    disabled,
-    errorMessage,
-    iconLeft,
-    iconRight,
-    label,
-    onChange,
-    onChangeValue,
-    onClickIconLeft,
-    onClickIconRight,
-    onEnter,
-    onKeyDown,
-    type,
-    ...restProps
-  } = props
-  const [showPassword, setShowPassword] = React.useState<boolean>(false)
-  const inputType = type === 'password' && showPassword ? 'text' : type
-  const switchShowPassword = () => {
-    setShowPassword(!showPassword)
-  }
+export const Input: React.FC = forwardRef<HTMLInputElement, InputProps>(
+  (props, ref): JSX.Element => {
+    const {
+      className,
+      disabled,
+      errorMessage,
+      iconLeft,
+      iconRight,
+      label,
+      onChange,
+      onChangeValue,
+      onClickIconLeft,
+      onClickIconRight,
+      onEnter,
+      onKeyDown,
+      type,
+      ...restProps
+    } = props
+    const [showPassword, setShowPassword] = React.useState<boolean>(false)
+    const inputType = type === 'password' && showPassword ? 'text' : type
+    const switchShowPassword = () => {
+      setShowPassword(!showPassword)
+    }
+    const switchButtonForPassword = showPassword ? <Eye /> : <EyeOff />
+    const onChangeValueHandler = (e: ChangeEvent<HTMLInputElement>) => {
+      onChange?.(e)
+      onChangeValue?.(e.currentTarget.value)
+    }
 
-  return (
-    <div>
-      {label && (
-        <Typography as={'label'} variant={'body2'}>
-          {label}
-        </Typography>
-      )}
-      <div className={''}>
-        <input type={inputType} {...restProps} />
-        {type === 'password' && <button onClick={switchShowPassword}></button>}
+    const onKeyDownHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+      if (onEnter && e.key === 'Enter') {
+        onEnter(e)
+      }
+      onKeyDown?.(e)
+    }
+
+    return (
+      <div>
+        {label && (
+          <Typography as={'label'} variant={'body2'}>
+            {label}
+          </Typography>
+        )}
+        <div className={''}>
+          <input
+            disabled={disabled}
+            onChange={onChangeValueHandler}
+            onKeyDown={onKeyDownHandler}
+            ref={ref}
+            type={inputType}
+            {...restProps}
+          />
+          {type === 'password' && (
+            <button onClick={switchShowPassword}>{switchButtonForPassword}</button>
+          )}
+          {iconRight && <button onClick={onClickIconRight}>{iconRight}</button>}
+          {type === 'search' && <button onClick={onClickIconLeft}>{<Search />}</button>}
+        </div>
+        {errorMessage && (
+          <Typography as={'span'} variant={'caption'}>
+            {errorMessage}
+          </Typography>
+        )}
       </div>
-      {errorMessage && (
-        <Typography as={'span'} variant={'caption'}>
-          {errorMessage}
-        </Typography>
-      )}
-    </div>
-  )
-}
+    )
+  }
+)
