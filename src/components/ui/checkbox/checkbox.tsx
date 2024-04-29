1edd02cfcc1d1c8326a9008ada0e762fa2192296
leonraz58 <leonraz58@gmail.com>
2024-04-30 02:58:51 +0300

    fix: Checkbox className prop

--- src/components/ui/checkbox/checkbox.tsx
+++ src/components/ui/checkbox/checkbox.tsx
@@ -3,6 +3,7 @@ import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
 import { CheckIcon } from '@/assets/icons/CheckIcon'
 import { Typography } from '@/components/ui/typography'
 import * as CheckboxRadix from '@radix-ui/react-checkbox'
+import { clsx } from 'clsx'
 
 import s from './checkbox.module.scss'
 
@@ -16,24 +17,25 @@ export type CheckboxProps = {
 } & ComponentPropsWithoutRef<typeof CheckboxRadix.Root>
 
 export const Checkbox = forwardRef<ElementRef<typeof CheckboxRadix.Root>, CheckboxProps>(
-  ({ disabled, errorMessage, id, label, ...rest }, ref) => {
+  ({ className, disabled, errorMessage, id, label, ...rest }, ref) => {
     return (
-      <div style={{ alignItems: 'center', display: 'flex' }}>
-        <div className={s.wrapper}>
-          <div className={s.around}>
-            <CheckboxRadix.Root
-              className={s.CheckboxRoot}
-              disabled={disabled}
-              id={id}
-              ref={ref}
-              {...rest}
-            >
-              <CheckboxRadix.Indicator className={s.CheckboxIndicator}>
-                <CheckIcon />
-              </CheckboxRadix.Indicator>
-            </CheckboxRadix.Root>
-          </div>
+      //<div style={{ alignItems: 'center', display: 'flex' }}>
+      <div className={clsx(s.wrapper, className)}>
+        {/*<div className={s.wrapper}>*/}
+        <div className={s.around}>
+          <CheckboxRadix.Root
+            className={s.CheckboxRoot}
+            disabled={disabled}
+            id={id}
+            ref={ref}
+            {...rest}
+          >
+            <CheckboxRadix.Indicator className={s.CheckboxIndicator}>
+              <CheckIcon />
+            </CheckboxRadix.Indicator>
+          </CheckboxRadix.Root>
         </div>
+        {/*</div>*/}
         <Typography as={'label'} htmlFor={id} variant={'body2'}>
           {label}
         </Typography>
