608a532e7db45fe2baa9bebcdecf5d6d3f26a528
Evgeny <info.elukas@gmail.com>
2024-04-30 19:28:06 +0300

    feat: add a validation scheme using zod #37

--- src/components/auth/sign-up-form /sign-up-form.tsx
+++ src/components/auth/sign-up-form /sign-up-form.tsx
@@ -1,14 +1,41 @@
-import { Input } from '@/components/ui/input'
 import { useForm } from 'react-hook-form'
+
 import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
 
 export type FormValues = {
   email: string
   pass: string
   passConfirm: string
 }
+const user: FormValues = {
+  email: '',
+  pass: '',
+  passConfirm: '',
+}
+const validationSchema = z
+  .object({
+    email: z.string().email().min(1, { message: 'email is required' }),
+    pass: z.string().min(1, { message: 'password is required' }),
+    passConfirm: z.string().min(1, { message: 'password is required' }),
+  })
+  .refine(data => data.pass === data.passConfirm, {
+    message: "Passwords don't match",
+    path: ['passConfirm'],
+  })
+
 export const SignUpForm = () => {
-  const { register, handleSubmit } = useForm<FormValues>()
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+    register,
+  } = useForm<FormValues>({
+    defaultValues: user,
+    resolver: zodResolver(validationSchema),
+  })
   const onSubmit = (data: FormValues) => {
     console.log(data)
   }
@@ -16,9 +43,13 @@ export const SignUpForm = () => {
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <Input {...register('email')} type={'text'} />
-        <Input {...register('pass')} type={'password'} />
-        <Input {...register('passConfirm')} type={'password'} />
+        <Input {...register('email')} errorMessage={errors.email?.message} type={'text'} />
+        <Input {...register('pass')} errorMessage={errors.pass?.message} type={'password'} />
+        <Input
+          {...register('passConfirm')}
+          errorMessage={errors?.passConfirm?.message}
+          type={'password'}
+        />
         <Button type={'submit'}>Submit</Button>
       </form>
     </>
