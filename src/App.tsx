90d8a610e1b13ad8c33431e7da2b5edf29e1c703
leonraz58 <leonraz58@gmail.com>
2024-05-12 03:11:14 +0300

    feat: add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import { TableHeader } from '@/components/ui/table/tableHeader'
+// import { TableHeader } from '@/components/ui/table/tableHeader'
+import { Router } from '@/utils/router'
 export type Column = {
   key: string
   sortable?: boolean
   title: string
 }
 export function App() {
-  const columns: Column[] = [
-    { key: '12', title: 'Name' },
-    { key: '34', title: 'Cards' },
-    { key: '45', title: 'Last Updated' },
-    { key: '67', title: 'Created by' },
-  ]
-
-  return (
-    <div>
-      <TableHeader columns={columns} />
-    </div>
-  )
+  // const columns: Column[] = [
+  //   { key: '12', title: 'Name' },
+  //   { key: '34', title: 'Cards' },
+  //   { key: '45', title: 'Last Updated' },
+  //   { key: '67', title: 'Created by' },
+  // ]
+  return <Router />
+  // return (
+  //   <div>
+  //     <TableHeader columns={columns} />
+  //   </div>
+  // )
 }
