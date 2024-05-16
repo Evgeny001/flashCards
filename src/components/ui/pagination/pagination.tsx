f9982840c49aa1d13df8c11327cd4d9b32677b3b
leonraz58 <leonraz58@gmail.com>
2024-05-17 00:32:20 +0300

    fix: remove total pages calculation from pagination

--- src/components/ui/pagination/pagination.tsx
+++ src/components/ui/pagination/pagination.tsx
@@ -18,7 +18,6 @@ export type PaginationProps = {
   className?: string
   currentPage: number
   onChangePage: (page: number) => void
-  pageSize: number
   siblingCount?: number
   totalCount: number
 } & PaginationConditionals
@@ -28,7 +27,6 @@ export const Pagination = ({
   currentPage,
   onChangePage,
   onPerPageChange,
-  pageSize,
   //perPage,
   perPageOptions,
   siblingCount = 1,
@@ -36,7 +34,6 @@ export const Pagination = ({
 }: PaginationProps) => {
   const paginationRange = usePagination({
     currentPage,
-    pageSize,
     siblingCount,
     totalCount,
   })
