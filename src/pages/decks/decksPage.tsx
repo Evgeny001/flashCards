a2cebef404905fe9748b449e0ecc2872e1ce7a6a
leonraz58 <leonraz58@gmail.com>
2024-05-17 03:47:18 +0300

    feat: add control panel to decks page

--- src/pages/decks/decksPage.tsx
+++ src/pages/decks/decksPage.tsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
+import { TrashOutline } from '@/assets/icons/TrashOutline'
 import { DecksTable } from '@/components/decks/decks-table'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import { Pagination } from '@/components/ui/pagination'
+import { Slider } from '@/components/ui/slider'
+import { Tabs } from '@/components/ui/tabs'
 import {
   useCreateDeckMutation,
   useDeleteDeckMutation,
@@ -11,15 +15,24 @@ import {
   useUpdateDeckMutation,
 } from '@/services/decks/decks.services'
 
+import s from './decksPage.module.scss'
+
 export function DecksPage() {
   const [search, setSearch] = useState('')
   const [currentPage, setCurrentPage] = useState(1)
+  const [minSlider, setMinSlider] = useState(0)
+  const [maxSlider, setMaxSlider] = useState(50)
+  const [itemsPerPage, setItemsPerPage] = useState(10)
+  const [tabValue, setTabValue] = useState('All Cards')
+
+  const authorId = tabValue === 'My Cards' ? 'f2be95b9-4d07-4751-a775-bd612fc9553a' : undefined
 
   const { data, error, isError, isLoading } = useGetDecksQuery({
+    authorId,
     currentPage: currentPage,
-    itemsPerPage: 20,
-    maxCardsCount: 10,
-    minCardsCount: 0,
+    itemsPerPage: itemsPerPage,
+    maxCardsCount: maxSlider,
+    minCardsCount: minSlider,
     name: search,
   })
 
@@ -39,53 +52,84 @@ export function DecksPage() {
   }
 
   return (
-    <div>
-      <Link to={'/login'}>login</Link>
-      <Input onChangeValue={setSearch} value={search} />
-      <Button
-        onClick={() =>
-          updateDeck({
-            id: 'clw89frlx02aho1015fc9zkrx',
-            name: 'new name',
-          })
-        }
-      >
-        updateDeck
-      </Button>
-      <Button
-        disabled={isDeckBeingCreated}
-        onClick={() =>
-          createDeck({
-            name: 'ololo',
-          })
-        }
-      >
-        create deck
-      </Button>
-      <Button
-        onClick={() => {
-          setCurrentPage(v => (v < 1 ? 1 : v - 1))
-        }}
-      >
-        -
-      </Button>
-      <Button
-        onClick={() => {
-          setCurrentPage(v => v + 1)
-        }}
-      >
-        +
-      </Button>
-      <DecksTable
-        decks={data?.items.map(deck => ({
-          cardsCount: deck.cardsCount,
-          createdBy: deck.author.name,
-          id: deck.id,
-          lastUpdated: deck.updated,
-          name: deck.name,
-        }))}
-        onDeleteClick={handleDeleteClick}
-      />
+    <div className={s.wrapper}>
+      <div>
+        <Link to={'/login'}>login</Link>
+      </div>
+
+      <div className={s.controlPanel}>
+        <Input onChangeValue={setSearch} value={search} />
+        <Tabs
+          className={s.tabs}
+          onValueChange={value => setTabValue(value)}
+          tabs={[
+            { disabled: false, value: 'My Cards' },
+            { disabled: false, value: 'All Cards' },
+          ]}
+          title={'Show decks cards'}
+          value={tabValue}
+        />
+        <Slider
+          onValueChange={value => {
+            setMinSlider(value[0])
+            setMaxSlider(value[1])
+          }}
+          value={[minSlider, maxSlider]}
+        />
+        <Button variant={'secondary'}>
+          <TrashOutline />
+          Clear Filter
+        </Button>
+      </div>
+      <div>
+        <DecksTable decks={data?.items} onDeleteClick={handleDeleteClick} />
+      </div>
+
+      <div className={s.paginationWrapper}>
+        <Pagination
+          currentPage={currentPage}
+          onChangePage={setCurrentPage}
+          onPerPageChange={value => setItemsPerPage(+value)}
+          perPageOptions={['5', '10', '20', '50']}
+          totalCount={50}
+        />
+      </div>
+      <div className={s.controlPanel}>
+        <Button
+          onClick={() =>
+            updateDeck({
+              id: 'clw89frlx02aho1015fc9zkrx',
+              name: 'new name',
+            })
+          }
+        >
+          updateDeck
+        </Button>
+        <Button
+          disabled={isDeckBeingCreated}
+          onClick={() =>
+            createDeck({
+              name: 'ololo',
+            })
+          }
+        >
+          create deck
+        </Button>
+        <Button
+          onClick={() => {
+            setCurrentPage(v => (v < 1 ? 1 : v - 1))
+          }}
+        >
+          -
+        </Button>
+        <Button
+          onClick={() => {
+            setCurrentPage(v => v + 1)
+          }}
+        >
+          +
+        </Button>
+      </div>
     </div>
   )
 }
