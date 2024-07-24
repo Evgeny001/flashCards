9dc71bc73a1f8f7ef0d4d7bf7f61bef8772e4140
Evgeny <info.elukas@gmail.com>
2024-07-24 15:48:32 +0300

    feat: add react-toastify lib for to display notifications and processed the information from the SignUp add SignUpErrorResponse type

--- src/pages/signUpPage/signUpPage.tsx
+++ src/pages/signUpPage/signUpPage.tsx
@@ -2,8 +2,10 @@ import { useNavigate } from 'react-router-dom'
 
 import { SignUpForm, SignUpFormValues } from '@/components/auth/sign-up-form'
 import { FormValues } from '@/components/auth/signIn'
+import { errorNotification, successNotification } from '@/lib/notifications'
 import { PageContainer } from '@/pages/pageContainer/pageContainer'
 import { useLoginMutation, useSignUpMutation } from '@/services/auth/auth.services'
+import { SignUpErrorResponse } from '@/services/auth/auth.types'
 
 export const SignUpPage = () => {
   const [signUp] = useSignUpMutation()
@@ -17,14 +19,20 @@ export const SignUpPage = () => {
   const handleSubmit = async (data: Omit<SignUpFormValues, 'sendConfirmationEmail'>) => {
     try {
       const { passwordConfirm, ...dataWithoutPasswordConfirm } = data
-      // Деструктурирую объект, оставляю только нужные свойства
-      await signUp(dataWithoutPasswordConfirm).unwrap()
 
+      // Деструктурирую объект, оставляю только нужные свойства
+      const res = await signUp(dataWithoutPasswordConfirm).unwrap()
+
+      successNotification(`${res.name}, you are successfully registered`)
       const dataForLogin: FormValues = { rememberMe: false, ...data }
 
       login(dataForLogin)
       navigate('/')
-    } catch {}
+    } catch (e) {
+      const signUpError = e as SignUpErrorResponse
+
+      errorNotification(signUpError.data.errorMessages[0])
+    }
   }
 
   return (
